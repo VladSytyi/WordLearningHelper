9919680b0a3d86fe4a007737061a7a00da509777
Vladislav Sytyi <valdis3gs@gmail.com>
2022-12-30 23:55:18 +0100

    feat(all): added random select and exit points

--- src/main/java/en/education/Main.java
+++ src/main/java/en/education/Main.java
@@ -3,6 +3,7 @@ package en.education;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.sql.SQLOutput;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -18,13 +19,63 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         String input = scan.nextLine();
+        maybeTurnOff(input);
+
+        if ( !init.keySet().contains(input)) {
+            System.out.println("try again");
+            System.exit(0);
+        }
+
 
         System.out.println("We will go through this topic " + input);
 
         List<Pair<String, String>> pairs = init.get(input);
 
-        System.out.println(pairs);
 
+
+        Pair<String, String> pair = randomValue(pairs);
+
+        boolean isEnglish = booleanRandom();
+
+        String question = engOrRus(pair, isEnglish);
+        String expectedAnswer = engOrRus(pair, !isEnglish);
+
+        System.out.println(question);
+
+        String answer = scan.nextLine();
+
+        maybeTurnOff(answer);
+
+        if (expectedAnswer.contains(answer)) {
+
+            System.out.println("Excellent!!!");
+
+            System.out.println(question);
+            System.out.println();
+            System.out.println(answer);
+        }
+
+    }
+
+    private static void maybeTurnOff(String string) {
+        if (string.contains("exit") || string.contains("quit")) {
+            System.out.println("GoodBye");
+            System.exit(0);
+        }
+    }
+
+    private static Pair<String, String> randomValue(List<Pair<String, String>> pairs) {
+        Random random = new Random();
+        return pairs.get(random.nextInt(pairs.size()));
+    }
+
+    private static boolean booleanRandom(){
+        Random random = new Random();
+        return random.nextBoolean();
+    }
+
+    private static String engOrRus(Pair<String, String> pair, boolean isEnglish) {
+        return (isEnglish) ? pair.first() : pair.second();
     }
 
 
@@ -42,18 +93,7 @@ public class Main {
     public static Map<String, List<Pair<String, String>>> init() {
         Map<String, List<Pair<String, String>>> mapa = new HashMap<>();
         paths().forEach(e -> mapa.put(Main.formattingFunction(e), Main.read(e)));
-
         return mapa;
-
-//        return paths()
-//                .stream()
-//                .collect(
-//                        Collectors.groupingBy(
-//                                Main::formattingFunction, Collectors.mapping(Main::read)
-//                        )
-//                );
-
-
     }
 
     public static List<Path> paths() {
